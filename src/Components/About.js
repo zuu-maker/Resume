3318e77b76fc2fd2906c87e2e71425c8a56553d1
Mkhuzo <zulumkhuzo@gmail.com>
2023-01-09 19:00:54 +0800

    fix hero image

---
+++ src/Components/About.js
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from "react";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
+const About = ({ data }) => {
+  const { ref, inView } = useInView({
+    threshold: 0.6,
+  });
+  const animatation = useAnimation();
+  const [done, setDone] = useState(false);
+
+  if (data) {
+    var name = data.name;
+    var profilepic = "images/" + data.image;
+    var bio = data.bio;
+    var street = data.address.street;
+    var city = data.address.city;
+    var state = data.address.state;
+    var zip = data.address.zip;
+    var phone = data.phone;
+    var email = data.email;
+    // var resumeDownload = data.resumedownload;
+  }
+
+  useEffect(() => {
+    if (!done && inView) {
+      animatation.start({
+        scale: [1, 1.2, 1.2, 1, 1],
+        rotate: [0, 0, 180, 180, 0],
+        transition: {
+          duration: 2.4,
+          ease: "easeInOut",
+          times: [0, 0.2, 0.5, 0.8, 1],
+        },
+      });
+      setDone(true);
+    }
+    // eslint-disable-next-line
+  }, [inView, animatation]);
+
+  return (
+    <section id="about">
+      <div ref={ref} className="row">
+        <div className="three columns">
+          <motion.img
+            animate={animatation}
+            className="profile-pic"
+            src={profilepic}
+            alt="Mkhuzo's Profile Pic"
+          />
+        </div>
+        <div className="nine columns main-col">
+          <h2>About Me</h2>
+
+          <p>{bio}</p>
+          <div className="row">
+            <div className="columns contact-details">
+              <h2>Contact Details</h2>
+              <p className="address">
+                <span>{name}</span>
+                <br />
+                <span>
+                  {street}
+                  <br />
+                  {city} {state}, {zip}
+                </span>
+                <br />
+                <span>{phone}</span>
+                <br />
+                <span>{email}</span>
+              </p>
+            </div>
+            {/* <div className="columns download">
+              <p>
+                <a href={resumeDownload} className="button">
+                  <i className="fa fa-download"></i>Download Resume
+                </a>
+              </p>
+            </div> */}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDDV51fBcQ-xEMHO4XU5rHhCvoBnAHxz5o",
+    authDomain: "resume-903bf.firebaseapp.com",
+    projectId: "resume-903bf",
+    storageBucket: "resume-903bf.appspot.com",
+    messagingSenderId: "780293687989",
+    appId: "1:780293687989:web:a35dfef4ff5ddc3260ccfc"
+  };
+
+  firebase.initializeApp(firebaseConfig);
