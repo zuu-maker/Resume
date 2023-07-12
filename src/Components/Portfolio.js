ab214afb53c5996184c91468265a3af5798d6ce0
Mkhuzo <zulumkhuzo@gmail.com>
2023-07-12 18:31:02 +0800

    add  document

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -41,12 +41,13 @@ const Portfolio = ({ data }) => {
       <div className="row">
         <div className="twelve columns collapsed">
           <h1>Check Out Some of My Works.</h1>
-
-          <div
-            id="portfolio-wrapper"
-            className="bgrid-quarters s-bgrid-thirds cf"
-          >
-            {projects}
+          <div className="portfolio_new">
+            <div
+              id="portfolio-wrapper"
+              className="bgrid-quarters s-bgrid-thirds cf"
+            >
+              {projects}
+            </div>
           </div>
         </div>
       </div>
